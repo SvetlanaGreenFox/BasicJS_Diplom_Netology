4ae8cba27b0eb14d35ac674ea682cbf104804003
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-07-15 17:03:04 +0300

    Add FormRegistration

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const userForm = new UserForm();
+
 userForm.loginFormCallback = data => {
     let result = ApiConnector.login(data, callback => console.log(callback));
     if (result.success) {
@@ -11,5 +12,13 @@ userForm.loginFormCallback = data => {
     )
 }
 
-
+userForm.registerFormCallback = data => {
+    let result = ApiConnector.register(data, callback => console.log(callback));
+    if (result.success) {
+        location.reload();
+    } else (
+        let message = result.error;
+    setRegisterErrorMessage(message);
+    )
+}
 
